aa88e8b599a9a13ec2678da286f9a0845ddefb46
Alae Azzaoui <125179905+AzzaouiAlae@users.noreply.github.com>
2025-11-16 15:02:51 +0100

    Add files via upload

---
+++ vscodeInstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir ~/vsCodeInstaller
+wget -O ~/vsCodeInstaller/vsCode.tar.gz  'https://code.visualstudio.com/sha/download?build=stable&os=linux-x64'
+tar -xvzf ~/vsCodeInstaller/vsCode.tar.gz -C ~/vsCodeInstaller
+
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "OpenVSCode"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "~/vsCodeInstaller/VSCode-linux-x64/bin/code"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Control><Alt>c"
