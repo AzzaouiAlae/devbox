5acc0173ab470316e4ea08bc31c27890cb854ac4
AzzaouiAlae <azzaoui@outlook.com>
2026-01-01 11:38:24 +0100

    update

--- vscodeInstaller.sh
+++ vscodeInstaller.sh
@@ -5,6 +5,6 @@ wget -O ~/vsCodeInstaller/vsCode.tar.gz  'https://code.visualstudio.com/sha/down
 tar -xvzf ~/vsCodeInstaller/vsCode.tar.gz -C ~/vsCodeInstaller
 
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "OpenVSCode"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "~//home/aazzaoui/vsCodeInstaller/VSCode-linux-x64/bin/code-tunnel"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "OpenVSCode2"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "$HOME/vsCodeInstaller/VSCode-linux-x64/bin/code-tunnel"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Control><Alt>c"
